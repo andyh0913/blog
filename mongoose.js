8aafd2aedc1bfc43768961f897e72c17cc43a057
andyh0913 <andyh0913@gmail.com>
2018-06-16 06:57:49 +0800

    use mlab

--- mongoose.js
+++ mongoose.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/');
+mongoose.connect('mongodb://web2018:web2018@ds159100.mlab.com:59100/blog');
 
 export {mongoose};
