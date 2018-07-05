a2790303ffa634e943de027d3ed3de53b41b5713
andyh0913 <andyh0913@gmail.com>
2018-07-05 13:41:00 +0800

    cors header

--- server.js
+++ server.js
@@ -12,6 +12,13 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+app.use('/', (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Content-Type")
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next()
+})
+
 app.post('/newpost',(req,res)=>{
   console.log(req.body);
   var article = new Article({
